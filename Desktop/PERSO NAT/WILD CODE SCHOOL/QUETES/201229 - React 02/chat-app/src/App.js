9c74f245f87f5176741c43dbf758d45fdaa92d00
Nathalie Crispin <n.crispin.digital@gmail.com>
2020-12-31 12:14:52 +0100

    React Q3

--- Desktop/PERSO NAT/WILD CODE SCHOOL/QUETES/201229 - React 02/chat-app/src/App.js
+++ Desktop/PERSO NAT/WILD CODE SCHOOL/QUETES/201229 - React 02/chat-app/src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import Contact from './components/Contact'
+import React from "react";
+import Contact from "./components/Contact";
+import PropTypes from 'prop-types';
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact
+        name="Anita Sutton"
+        avatar="https://randomuser.me/api/portraits/men/87.jpg"
+        statusText="Online" />
+      <Contact
+        name="Jim George"
+        avatar="https://randomuser.me/api/portraits/men/98.jpg"
+        statusText= "Offline"/>
+      <Contact
+        name="Charlotte Ryan"
+        avatar="https://randomuser.me/api/portraits/women/6.jpg"
+        statusText="Online" />
     </div>
   );
 }
+
+Greeting.propTypes = {
+  name: PropTypes.string
+};
 
 export default App;
--- Desktop/PERSO NAT/WILD CODE SCHOOL/QUETES/201229 - React 02/chat-app/src/components/Contact.js
+++ Desktop/PERSO NAT/WILD CODE SCHOOL/QUETES/201229 - React 02/chat-app/src/components/Contact.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
+import PropTypes from 'prop-types';
 
-function Contact() {
-    return(
-        <div className="contact">
-            
-            <img src="https://randomuser.me/api/portraits/men/28.jpg" alt="avatar" />
-            <div>
-                <h1>Eduardo Stephens</h1>
-                <div className="status">
-                    <img src="" alt="" />
-                    <p className="status-online">Online</p>
-                </div>
-            </div>
+
+function Contact(props) {
+  return (
+    <div className="Contact">
+      <img
+        className="avatar"
+        src={props.avatar}
+        alt={props.name}
+      />
+      <div>
+        <p className="name">{props.name}</p>
+        <div className="status">
+          <div className={props.statusText === "Online" ? "status-online" : "status-offline"} />
+          <p className="status-text">{props.statusText}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
+
+Contact.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  statusText: PropTypes.string.isRequired,
+};
 
 export default Contact;
